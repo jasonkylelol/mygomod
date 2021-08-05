5feefd5dbe5f808dd02cc03107d48d5334098e0e
jasonjiao <jasonjiao@tencent.com>
2021-08-05 17:36:51 +0800

    init repo

--- coin_exchange.go
+++ coin_exchange.go
@@ -32,7 +32,31 @@ func dpfunc(coins []int, amount int, dpMap *map[int]int) int {
 	return val
 }
 
+func dpfuncNoRecurse(coins []int, amount int) int {
+	dplist := []int{}
+	for i := 0; i < amount+1; i++ {
+		dplist = append(dplist, amount+1)
+	}
+	dplist[0] = 0
+	for idx := range dplist {
+		for _, coin := range coins {
+			if idx-coin < 0 {
+				continue
+			}
+			if dplist[idx] > 1+dplist[idx-coin] {
+				dplist[idx] = 1 + dplist[idx-coin]
+			}
+		}
+		fmt.Println(dplist)
+	}
+	if dplist[amount] == amount+1 {
+		return -1
+	}
+	return dplist[amount]
+}
+
 func coinChange(coins []int, amount int) int {
-	dpMap := map[int]int{}
-	return dpfunc(coins, amount, &dpMap)
+	// dpMap := map[int]int{}
+	// return dpfunc(coins, amount, &dpMap)
+	return dpfuncNoRecurse(coins, amount)
 }
