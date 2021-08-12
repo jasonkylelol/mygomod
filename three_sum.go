6e6909eee01142f88fcb3c0c2bfd5c6cfad00478
jasonjiao <jasonjiao@tencent.com>
2021-08-12 11:51:19 +0800

    init repo

--- three_sum.go
+++ three_sum.go
@@ -1,6 +1,59 @@
 package mygomod
 
+import "sort"
+
+func innerTwoSum(nums []int, start, target int) [][]int {
+	low := start
+	high := len(nums) - 1
+	res := [][]int{}
+	for low < high {
+		lval, hval := nums[low], nums[high]
+		sum := lval + hval
+		if sum < target {
+			low++
+		} else if sum > target {
+			high--
+		} else {
+			res = append(res, []int{nums[low], nums[high]})
+			for low < high {
+				if lval != nums[low] {
+					break
+				}
+				low++
+			}
+			for low < high {
+				if hval != nums[high] {
+					break
+				}
+				high--
+			}
+		}
+	}
+	return res
+}
+
 func threeSum(nums []int) [][]int {
 	res := [][]int{}
+	if len(nums) == 0 {
+		return res
+	}
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	for idx := 0; idx < len(nums); idx++ {
+		val := nums[idx]
+		target := 0 - val
+		twosumlist := innerTwoSum(nums, idx+1, target)
+		for _, twosums := range twosumlist {
+			twosums = append(twosums, val)
+			res = append(res, twosums)
+		}
+		for idx < len(nums)-1 {
+			if nums[idx] != nums[idx+1] {
+				break
+			}
+			idx++
+		}
+	}
 	return res
 }
--- three_sum_test.go
+++ three_sum_test.go
@@ -1,1 +1,31 @@
 package mygomod
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type threeSumParam struct {
+	Nums []int
+	Ret  [][]int
+}
+
+var threeSumParams = []threeSumParam{
+	{
+		Nums: []int{-1, 0, 1, 2, -1, -4},
+		Ret: [][]int{
+			{-1, -1, 2},
+			{-1, 0, 1},
+		},
+	},
+}
+
+func Test_threeSum(t *testing.T) {
+	for _, param := range threeSumParams {
+		t.Run("", func(t *testing.T) {
+			ret := threeSum(param.Nums)
+			assert.Equal(t, param.Ret, ret)
+		})
+	}
+}
