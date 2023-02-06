a38d1478146092a315a86b6aa8574a1282d95e8c
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2023-02-06 18:29:49 +0800

    remove the n-th node from the end of the list

--- remove_nth_node_from_end_of_list.go
+++ remove_nth_node_from_end_of_list.go
@@ -1,7 +1,5 @@
 package mygomod
 
-import "fmt"
-
 // Given the head of a linked list, remove the n-th node from the end of the list and return its head.
 // Input: head = [1,2,3,4,5], n = 2
 // Output: [1,2,3,5]
@@ -12,7 +10,6 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	for fast != nil {
 		fast = fast.Next
 		if steps > n {
-			fmt.Println(steps, slow.Val)
 			slow = slow.Next
 		}
 		steps++
