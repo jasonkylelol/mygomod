d42c04f9034f8ab5955e549e8f0b230158d6ac0e
jasonkylelol <26690662+jasonkylelol@users.noreply.github.com>
2023-02-09 11:49:16 +0800

    update

--- merge_two_sorted_lists_test.go
+++ merge_two_sorted_lists_test.go
@@ -6,8 +6,6 @@ func Test_mergeTwoLists(t *testing.T) {
 	t.Run("", func(t *testing.T) {
 		list1 := convLinkedList([]int{1, 2, 4, 5, 9})
 		list2 := convLinkedList([]int{1, 3, 4, 7, 13})
-		// list1 := convLinkedList([]int{})
-		// list2 := convLinkedList([]int{0})
 		res := mergeTwoLists(list1, list2)
 		printLinkedList(res)
 	})
